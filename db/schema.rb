3bd8f9310044351060ecaa19eeb61f92692bbf6d
elbincodes <elbincodes@gmail.com>
2017-10-26 14:19:41 -0400

    pushing to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023184658) do
+ActiveRecord::Schema.define(version: 20171025212450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.text "content"
     t.integer "restaurant_id"
-    t.integer "user_id"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -34,11 +34,14 @@ ActiveRecord::Schema.define(version: 20171023184658) do
     t.string "name"
     t.string "address"
     t.float "rating"
-    t.string "img_url"
     t.float "longitude"
     t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.string "url"
+    t.string "categories"
+    t.string "phone"
   end
 
   create_table "users", force: :cascade do |t|
